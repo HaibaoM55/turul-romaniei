9481c38d2f066e6f54e1e27d2e958a7f4aa2d795
HaibaoM55 <96226102+HaibaoM55@users.noreply.github.com>
2022-07-02 09:23:02 +0300

    initial

--- joc.js
+++ joc.js
@@ -25,12 +25,6 @@ function zar(){
         old_poz = poz_j1;
         poz_j1 += zar_nr1;
         poz_j1 += zar_nr2;
-        // if(poz_j1 > 12){
-        //     neupoz_j1 = poz_j1 - 12;
-        //     setTimeout(() => {animhigh(old_poz, 12);}, 2000);
-        //     setTimeout(() => {animhigh(0, neupoz_j1);}, 2000 + (poz_j1 - old_poz) * 100 + 200);
-        //     poz_j1 = poz_j1 - 12;
-        // }
         vewold_poz = poz_j1;
         if(poz_j1 > 13)
         {
@@ -45,7 +39,39 @@ function zar(){
             setTimeout(() => {animhigh(old_poz, poz_j1);}, 2000);
             setTimeout(() => {j1m_anim(old_poz, poz_j1)}, 2000);
         }
+        setTimeout(() => {
+            jucator1 = false;
+            jucator2 = true; 
+            $("anunt").style.display = "block";
+            $("anunt_text").innerText = "Jucatorul 2 a luat zarul";           
+        }, 4000);
     }
+    if(jucator2){
+        old_poz = poz_j2;
+        poz_j2 += zar_nr1;
+        poz_j2 += zar_nr2;
+        vewold_poz = poz_j2;
+        if(poz_j2 > 13)
+        {
+            poz_j2 = poz_j2 - 14;
+            setTimeout(() => {animhigh(old_poz, 13);}, 2000);
+            setTimeout(() => {animhigh(0, poz_j2);}, 4000);
+            setTimeout(() => {j2m_anim(old_poz, 13)}, 2000);
+            setTimeout(() => {j2m_anim(0, poz_j2)}, 4000);
+        }
+        else
+        {
+            setTimeout(() => {animhigh(old_poz, poz_j2);}, 2000);
+            setTimeout(() => {j2m_anim(old_poz, poz_j2)}, 2000);
+        }
+        setTimeout(() => {
+            jucator1 = true;
+            jucator2 = false;
+            $("anunt").style.display = "block";
+            $("anunt_text").innerText = "Jucatorul 1 a luat zarul";
+        }, 4000);
+    }
+    setTimeout(() => {$("anunt").style.display = "none";}, 5000);
     setTimeout(() => {$("zar").style.display = "none";}, 2000);
 }
 function highmergi(numar){
@@ -174,5 +200,68 @@ function animhigh(start, sfarsit){
 function j1m_anim(start, sfarsit){
     for(let i = start; i <= sfarsit; i++){
         setTimeout(() => {mergi_j1(i);}, i*100);
+    }
+}
+function mergi_j2(poz_j2){
+    if(poz_j2 == 0){
+        $("text_j2").style.top = "280px";
+        $("text_j2").style.left = "400px";
+    }
+    if(poz_j2 == 1){
+        $("text_j2").style.top = "280px";
+        $("text_j2").style.left = "310px";
+    }
+    if(poz_j2 == 2){
+        $("text_j2").style.top = "280px";
+        $("text_j2").style.left = "200px";
+    }
+    if(poz_j2 == 3){
+        $("text_j2").style.top = "280px";
+        $("text_j2").style.left = "100px";
+    }
+    if(poz_j2 == 4){
+        $("text_j2").style.top = "280px";
+        $("text_j2").style.left = "0px";
+    }
+    if(poz_j2 == 5){
+        $("text_j2").style.top = "180px";
+        $("text_j2").style.left = "0px";
+    }
+    if(poz_j2 == 6){
+        $("text_j2").style.top = "80px";
+        $("text_j2").style.left = "0px";
+    }
+    if(poz_j2 == 7){
+        $("text_j2").style.top = "0px";
+        $("text_j2").style.left = "0px";
+    }
+    if(poz_j2 == 8){
+        $("text_j2").style.top = "0px";
+        $("text_j2").style.left = "110px";
+    }
+    if(poz_j2 == 9){
+        $("text_j2").style.top = "0px";
+        $("text_j2").style.left = "210px";
+    }
+    if(poz_j2 == 10){
+        $("text_j2").style.top = "0px";
+        $("text_j2").style.left = "310px";
+    }
+    if(poz_j2 == 11){
+        $("text_j2").style.top = "0px";
+        $("text_j2").style.left = "400px";
+    }
+    if(poz_j2 == 12){
+        $("text_j2").style.top = "80px";
+        $("text_j2").style.left = "400px";
+    }
+    if(poz_j2 == 13){
+        $("text_j2").style.top = "180px";
+        $("text_j2").style.left = "400px";
+    }
+}
+function j2m_anim(start, sfarsit){
+    for(let i = start; i <= sfarsit; i++){
+        setTimeout(() => {mergi_j2(i);}, i*100);
     }
 }
